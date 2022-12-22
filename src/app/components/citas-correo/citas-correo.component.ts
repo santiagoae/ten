3ae359b93aac6f1591f0920c018ddb4d1dd3b5e7
practicantestigo <tigounepracticantes@gmail.com>
2022-12-22 15:56:30 -0500

    cambio carousel, especialistas ajuste y boton preguntas

--- src/app/components/citas-correo/citas-correo.component.ts
+++ src/app/components/citas-correo/citas-correo.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as moment from 'moment';
 import { NgToastService } from 'ng-angular-popup';
 import { correoModel } from 'src/app/models/correo.model.js';
 import { CorreoService } from 'src/app/services/correo.service';
-import {Email} from '../../../assets/js/smtp';
+// import {Email} from '../../../assets/js/smtp';
 
 
 @Component({
@@ -14,58 +14,57 @@ import {Email} from '../../../assets/js/smtp';
 })
 export class CitasCorreoComponent implements OnInit {
 
-  // citas: FormGroup;
+  citas: FormGroup;
   fechaActual: any;
   correoModel: correoModel;
 
   constructor(
-    // private fb: FormBuilder,
+    private fb: FormBuilder,
               private alerts: NgToastService,
               private correoSvc: CorreoService) { }
 
   ngOnInit(): void {
-    // this.crearFormulario();
+    this.crearFormulario();
     this.fechaActual = moment().format("YYYY-MM-DD");
   }
 
 
-  // crearFormulario(){
-  //   this.citas = this.fb.group({
-  //     name: ['', Validators.required],
-  //     email: ['', Validators.required],
-  //     phone: ['', Validators.required],
-  //     service: ['', Validators.required],
-  //     tentative_date: ['', Validators.required],
-  //   })
-  // }
+  crearFormulario(){
+    this.citas = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      phone: ['', Validators.required],
+      service: ['', Validators.required],
+      tentative_date: ['', Validators.required],
+    })
+  }
 
-  // agendar(){
-  //   this.correoSvc.agendarCita(this.citas.value).subscribe({
-  //     next: res => {
-  //       this.alerts.success({detail: res.response, summary: 'Te responderemos a ' + this.citas.get('email')?.value, duration:6000});
-  //       console.log(this.citas.value)
-  //       this.citas.reset();
-  //     },
-  //     error: err => {
-  //       this.alerts.error({detail: err.error, summary: 'intentalo de nuevo', duration:6000});
-  //     }
-  //   })
-  // }
+  agendar(){
+    this.correoSvc.agendarCita(this.citas.value).subscribe({
+      next: res => {
+        this.alerts.success({detail: res.response, summary: 'Te responderemos a ' + this.citas.get('email')?.value, duration:6000});
+        console.log(this.citas.value)
+        this.citas.reset();
+      },
+      error: err => {
+        this.alerts.error({detail: err.error, summary: 'intentalo de nuevo', duration:6000});
+      }
+    })
+  }
 
   // agendar() {
-  //   this. correoModel = this.citas.value;
-  //   console.log(this.correoModel);
-  //   Email.send({
-  //   Host : 'smtp.elasticemail.com',
-  //   Username : 'kawadsign@gmail.com',
-  //   Password : '94EC21B627E0E5E351B5CE08477B3774A639',
-  //   To : 'citas@tensaludoral.com',
-  //   From : `kawadsign@gmail.com`,
-  //   Subject : this.correoModel.service,
-  //   Body : `
-  //   <br/> <b>Name: </b>${this.correoModel.name} <br /> <b>Email: </b>${this.correoModel.email}<br /> <b>Phone: </b>${this.correoModel.phone}<br /> <b>Subject: </b>${this.correoModel.service}<br /> <b>Tentative_Date:</b> <br /> ${this.correoModel.tentative_date} <br><br> <b>~End of Message.~</b> `
-  //   }).then( message => {alert(message); this.citas.reset(); } );
-
+  //     this. correoModel = this.citas.value;
+  //     console.log(this.correoModel);
+  //     Email.send({
+  //     Host : 'smtp.elasticemail.com',
+  //     Username : 'kawadsign@gmail.com',
+  //     Password : '94EC21B627E0E5E351B5CE08477B3774A639',
+  //     To : 'citas@tensaludoral.com',
+  //     From : `kawadsign@gmail.com`,
+  //     Subject : this.correoModel.service,
+  //     Body : `
+  //     <br/> <b>Name: </b>${this.correoModel.name} <br /> <b>Email: </b>${this.correoModel.email}<br /> <b>Phone: </b>${this.correoModel.phone}<br /> <b>Subject: </b>${this.correoModel.service}<br /> <b>Tentative_Date:</b> <br /> ${this.correoModel.tentative_date} <br><br> <b>~End of Message.~</b> `
+  //     }).then( message => {alert(message); this.citas.reset(); } );
   //   }
 
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api: 'https://tensaludoral.com/ten/BackTen'
+  api: 'https://tensaludoral.com/BackTen'
 };
